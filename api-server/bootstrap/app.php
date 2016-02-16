0234c4472f379668c7c60736a0d4bfb80a755ff7
Hyunju Choi <blackpost38@gmail.com>
2016-02-16 18:22:20 +0900

    불필요한 파일 제거

--- api-server/bootstrap/app.php
+++ api-server/bootstrap/app.php
@@ -94,17 +94,8 @@ $app->register(Dingo\Api\Provider\LumenServiceProvider::class);
 |
 */
 
-$app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
-    require __DIR__.'/../app/Http/routes.php';
+$app->group([], function () use ($app) {
+    require __DIR__.'/../app/Api/V1/routes.php';
 });
-
-// $api = app('Dingo\Api\Routing\Router');
-// $api->version('v1', function ($api) {
-//     echo 'route..';
-//     $api->get('collections/{collection}', 'App\Api\V1\Controllers\CollectionsController@show');
-//     // $api->get('collections/{collection}', function () {
-//     //     return 'test';
-//     // });
-// });
 
 return $app;
