3243b65cfaf72da052c374c65d04f66096bcda87
Hyunju Choi <hj.choi@yellotravel.com>
2016-02-29 12:10:16 +0900

    Adding ProductList component to extract the product paginate from ProductList component

---
+++ node-server/public/js/components/ProductPaginate.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const getPages = (paginate) => {
+    if (typeof paginate === 'undefined') {
+        return [];
+    }
+
+    // getting current page
+    const currentPage = paginate.current_page;
+    const lastPage = paginate.last_page;
+    let previousPage = [];
+    let nextPage = [];
+
+    // if current page - 1 or - 2 is less than 0
+    if (currentPage - 1 > 0) {
+        previousPage = [ currentPage - 1, ...previousPage ];
+    }
+    if (currentPage - 2 > 0) {
+        previousPage = [ currentPage - 2, ...previousPage ];
+    }
+    // result array is [current, current + 1, current + 2]
+
+    // if current page + 1 or +2 is greater than last page
+    if (currentPage + 1 <= lastPage) {
+        nextPage = [ ...nextPage, currentPage + 1 ];
+    }
+    if (currentPage + 2 <= lastPage) {
+        nextPage = [ ...nextPage, currentPage + 2 ];
+    }
+
+    // result array is [current - 2, current -1, current]
+    return [ ...previousPage, currentPage, ...nextPage ];
+};
+
+const ProductPaginate = ({
+    fetchProducts,
+    paginate
+}) => {
+    const pages = getPages(paginate);
+    return(
+        <div>
+            <ul className={'pagination'}>
+                {
+                    paginate && paginate.prev_page_url ?
+                        <li>
+                            <a href="#" aria-label="Previous"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    fetchProducts(paginate.current_page - 1);
+                                }}
+                            >
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        :
+                        ''
+                }
+
+                {
+                    pages ?
+                        pages.map((page, index) =>
+                            <li key={index}>
+                                <a href="#"
+                                    style={{
+                                        textDecoration: page !== paginate.current_page ? 'none' : 'underline'
+                                    }}
+                                    onClick={(e)=>{
+                                        e.preventDefault();
+                                        fetchProducts(page);}}
+                                >
+                                    { page }
+                                </a>
+                            </li>
+                        ) :
+                        ''
+                }
+
+                {
+                    paginate && paginate.current_page !== paginate.last_page ?
+                        <li>
+                            <a href="#" aria-label="Next"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    fetchProducts(paginate.current_page + 1);
+                                }}
+                            >
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                        :
+                        ''
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default ProductPaginate;
--- node-server/public/js/containers/ProductList.js
+++ node-server/public/js/containers/ProductList.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { selectedProduct } from '../actions/ActionsCreator';
 import products from '../actions/products';
 
+import ProductPaginate from '../components/ProductPaginate';
 import ProductItem from '../components/ProductItem';
 
 export class ProductList extends Component {
@@ -27,9 +28,7 @@ export class ProductList extends Component {
     render(){
 
         // const { keyword, products } = this.context.store.getState();
-        const { fetchProducts, visibleProducts, onProductClick } = this.props;
-
-        const { paginate, pages } = this.props;
+        const { fetchProducts, onProductClick, paginate, visibleProducts } = this.props;
 
         return(
             <div>
@@ -43,60 +42,10 @@ export class ProductList extends Component {
                         )
                     }
                 </ul>
-                <div>
-                    <ul className={'pagination'}>
-                        {
-                            paginate && paginate.prev_page_url ?
-                                <li>
-                                    <a href="#" aria-label="Previous"
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            fetchProducts(paginate.current_page - 1);
-                                        }}
-                                    >
-                                        <span aria-hidden="true">&laquo;</span>
-                                    </a>
-                                </li>
-                                :
-                                ''
-                        }
-
-                        {
-                            pages ?
-                                pages.map((page, index) =>
-                                    <li key={index}>
-                                        <a href="#"
-                                            style={{
-                                                textDecoration: page !== paginate.current_page ? 'none' : 'underline'
-                                            }}
-                                            onClick={(e)=>{
-                                                e.preventDefault();
-                                                fetchProducts(page);}}
-                                        >
-                                            { page }
-                                        </a>
-                                    </li>
-                                ) :
-                                ''
-                        }
-
-                        {
-                            paginate && paginate.current_page !== paginate.last_page ?
-                                <li>
-                                    <a href="#" aria-label="Next"
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            fetchProducts(paginate.current_page + 1);
-                                        }}
-                                    >
-                                        <span aria-hidden="true">&raquo;</span>
-                                    </a>
-                                </li>
-                                :
-                                ''
-                        }
-                    </ul>
-                </div>
+                <ProductPaginate
+                    fetchProducts={fetchProducts}
+                    paginate={paginate}
+                />
             </div>
         );
     }
@@ -110,45 +59,13 @@ const mapStateToProductListProps = (state) => {
     //     [ state.paginate.current_page - 1, state.paginate.current_page, state.paginate.current_page + 1] :
     //     [ ];
 
-    const paginate = state.paginate;
-    const pages = typeof paginate !== 'undefined' ?
-                    (function (paginate) {
-                        let result = [];
-                        // getting current page
-                        const currentPage = paginate.current_page;
-                        const lastPage = paginate.last_page;
-                        let previousPage = [];
-                        let nextPage = [];
-                        // if current page - 1 or - 2 is less than 0
-                        if (currentPage - 1 > 0) {
-                            previousPage = [ currentPage - 1, ...previousPage ];
-                        }
-                        if (currentPage - 2 > 0) {
-                            previousPage = [ currentPage - 2, ...previousPage ];
-                        }
-                        // result array is [current, current + 1, current + 2]
-
-                        // if current page + 1 or +2 is greater than last page
-                        if (currentPage + 1 <= lastPage) {
-                            nextPage = [ ...nextPage, currentPage + 1 ];
-                        }
-                        if (currentPage + 2 <= lastPage) {
-                            nextPage = [ ...nextPage, currentPage + 2 ];
-                        }
-
-                        // result array is [current - 2, current -1, current]
-                        return [ ...previousPage, currentPage, ...nextPage ];
-                    })(paginate) :
-                    [];
-
     return {
         // keyword에 해당되는 product를 filtering 하고, 만약 keyword가 없으면 모든 product를 보여 준다.
         visibleProducts: state.keyword !== '' ?
                             state.products.filter((product) => {
                                 return product.name.indexOf(state.keyword) >= 0}
                             ) : state.products,
-        paginate: state.paginate,
-        pages
+        paginate: state.paginate
     };
 };
 
