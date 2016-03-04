0f4eb1ac07aa0f878048d69d3c75028782b82e3f
Hyunju Choi <hj.choi@yellotravel.com>
2016-03-04 11:45:43 +0900

    I tested ProductInfo component with enzyme

---
+++ test/components/ProductInfo.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+
+import ProductInfo from '../../clients/src/components/ProductInfo';
+
+describe('ProductInfo component', () => {
+    let props, wrapper;
+    before(() => {
+        props = {
+            selectedProduct: {
+                name: '룰루랄라',
+                desc: '아이후헤호하히하후호힝라두팓'
+            }
+        };
+        wrapper = shallow(<ProductInfo {...props} />);
+    });
+    it('should render element with product info data', () => {
+        expect(
+            wrapper.find('h1').text()
+        ).to.be.equal(props.selectedProduct.name);
+
+        expect(
+            wrapper.find('p').text()
+        ).to.be.equal(props.selectedProduct.desc);
+    });
+});
