b656d5d8e11e7faa6ddffa6787db81130e6ddd56
Hyunju Choi <hj.choi@yellotravel.com>
2016-03-04 14:25:42 +0900

    I modified productinfo component test code

--- test/components/ProductInfo.spec.js
+++ test/components/ProductInfo.spec.js
@@ -8,20 +8,18 @@ describe('ProductInfo component', () => {
     let props, wrapper;
     before(() => {
         props = {
-            selectedProduct: {
-                name: '룰루랄라',
-                desc: '아이후헤호하히하후호힝라두팓'
-            }
+            name: '룰루랄라',
+            desc: '아이후헤호하히하후호힝라두팓'
         };
         wrapper = shallow(<ProductInfo {...props} />);
     });
     it('should render element with product info data', () => {
         expect(
             wrapper.find('h1').text()
-        ).to.be.equal(props.selectedProduct.name);
+        ).to.be.equal(props.name);
 
         expect(
             wrapper.find('p').text()
-        ).to.be.equal(props.selectedProduct.desc);
+        ).to.be.equal(props.desc);
     });
 });
